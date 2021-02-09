1994244848b2f287143d51a87cc403d50959e85a
Alaa Elsaka <alaa.elsaka@cognizant.com>
2021-02-08 20:48:01 -0600

    project setup

---
+++ src/test/java/com/java/dummy/trials/TrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dummy.trials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
